21a022cf364b9ccbce2f78bdcebb70aed2bce474
Zakhar Molchanov <zvmolchanov@edu.hse.ru>
2024-06-16 23:52:44 +0500

    Добавлены роуты для регистрации и авторизации.  Для аутентификации ..

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,11 +2,13 @@ import { RootController } from "./controllers/RootController";
 import { HelloWorldController } from "./controllers/HelloWorldController";
 import { app } from "./server";
 import { UserController } from "./controllers/UserController";
+import { AuthController } from "./controllers/AuthController";
 
 export const registerControllers = () => {
   app.get("/helloWorld", HelloWorldController);
   app.get("/", RootController);
-
+  app.post("/api/auth/register", AuthController.register);
+  app.post("/api/auth/login", AuthController.login);
   app.get("/user", UserController);
   app.post("/user", UserController);
 };
