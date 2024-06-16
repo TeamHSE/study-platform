719dc010590bacad44d1aa2e914f627d414da45b
aleks <aleks@noreply.com>
2024-06-16 12:24:18 +0500

    feat: add demo account layout, api-service & courses page

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,22 +1,20 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
+import React from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: [ "cyrillic", "latin" ] });
 
 export const metadata: Metadata = {
-  title: "Обучающая платформа",
-  description: "Учитесь и обучайте других",
+  title: "Платформа для тренировок",
+  description: "Тренируйтесь и тренируйте других"
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <body className={ inter.className }>{ children }</body>
     </html>
   );
 }
