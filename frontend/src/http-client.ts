96c1d8bd8c183a5fe3ca612b2d148820e05046e2
aleks <aleks@noreply.com>
2024-06-19 20:33:07 +0500

    feat(http): add toast when 422 api response with msgs

--- frontend/src/http-client.ts
+++ frontend/src/http-client.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { getAccessToken, removeFromStorage } from "@/services/auth-token.service";
+import { toast } from "sonner";
 
 const options = {
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -43,6 +44,17 @@ http.interceptors.response.use(
           removeFromStorage();
         }
       }
+    }
+
+    if (error?.response?.status === 422) {
+      if (error.response.data.errors) {
+        const validationErrors = error.response.data.errors.map((e: any) => e.msg);
+        toast.error(`Ошибки валидации:\n${ validationErrors.join("\n") }`,
+          { duration: 10000, closeButton: true, important: true });
+      } else {
+        toast.error("Ошибки валидации");
+      }
+      return;
     }
 
     throw error;
