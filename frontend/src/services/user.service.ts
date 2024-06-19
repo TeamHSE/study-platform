e855a5033fbad7b826e6469c9a992155df1463c4
aleks <aleks@noreply.com>
2024-06-19 20:32:28 +0500

    fix(api): add user update api fetch

--- frontend/src/services/user.service.ts
+++ frontend/src/services/user.service.ts
@@ -2,23 +2,13 @@ import { IUser } from "@/types/auth.types";
 import { http } from "@/http-client";
 
 export const userService = {
-  async getUser(): Promise<IUser> {
+  async getUser(): Promise<IUser | null> {
     const response = await http.get<IUser>(`/users/`);
-    return response.data;
+    return response?.data;
   },
 
-  async updateUser(userId: string, login: string, role: string, name: string, password: string) {
-    const response = await http.put(`/users/${ userId }`, {
-      login,
-      role,
-      name,
-      password
-    });
-    return response.data;
-  },
-
-  async deleteUser(userId: string) {
-    const response = await http.delete(`/users/${ userId }`);
-    return response.status;
+  async updateUser(newUser: IUser): Promise<IUser | null> {
+    const response = await http.put("/users/", newUser);
+    return response?.data;
   }
 };
