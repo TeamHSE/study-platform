1e234b0c767d6f5958979b1343489c606ba26073
Zakhar Molchanov <zvmolchanov@edu.hse.ru>
2024-06-19 18:06:09 +0500

    feat(backend): add endpoint "Update User"

--- backend/src/middlewares/validation.ts
+++ backend/src/middlewares/validation.ts
@@ -17,3 +17,40 @@ export const userValidationRules = () => {
       .withMessage("Фамилия не может быть пустой!"),
   ];
 };
+
+export const userValidationDescription = () => {
+  return [
+    check("login").isEmail().withMessage("Неверный формат почты!"),
+    check("username")
+      .not()
+      .isEmpty()
+      .withMessage("Имя пользователя не может быть пустым!"),
+    check("firstName").not().isEmpty().withMessage("Имя не может быть пустым!"),
+    check("lastName")
+      .not()
+      .isEmpty()
+      .withMessage("Фамилия не может быть пустой!"),
+    check("isMale")
+      .optional()
+      .isBoolean()
+      .withMessage("Пол должен быть указан как булевое значение!"),
+    check("birthDate")
+      .optional()
+      .isISO8601()
+      .toDate()
+      .withMessage("Неверный формат даты!"),
+    check("weight").optional().isFloat().withMessage("Вес должен быть числом!"),
+    check("height")
+      .optional()
+      .isFloat()
+      .withMessage("Рост должен быть числом!"),
+    check("achievements")
+      .optional()
+      .isString()
+      .withMessage("Достижения должны быть строкой!"),
+    check("healthIssues")
+      .optional()
+      .isString()
+      .withMessage("Проблемы со здоровьем должны быть строкой!"),
+  ];
+};
